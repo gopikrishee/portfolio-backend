a3df985ae7977aa3f4bd4e4c32a1e6ac9ebef85b
GOPI KRISHNAN S <gopi@clubops.com.au>
2026-05-08 22:37:13 +0530

    Total blogs property added

---
+++ src/Models/DTOs/UserDto.cs
@@ -0,0 +1,15 @@
+namespace myprofile_backend.Models.DTOs
+{
+    public class UserDto
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? AvatarUrl { get; set; }
+        public string? Bio { get; set; }
+        public string? Title { get; set; }
+        public string? Location { get; set; }
+        public List<string>? Skills { get; set; }
+        public int TotalBlogs { get; set; }
+    }
+}
